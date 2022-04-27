ceced7d03995bce2a69db49487c254e0cb0f1950
Antonio Avila <v-antonio.avila@aceable.com>
2022-04-27 09:40:11 -0500

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,19 @@
 function updateThermo(params) {
-   // updateThermoProgress();
+   updateThermoProgress();
 }
 
 
 function updateThermoProgress() {
     const text = document.querySelector('.progress-label');
     const progress = document.querySelector('.progress');
-    let val = 40.00;
-    const initTime = new Date().setHours(10, 00);
+    let val = 80.93;
+    const initTime = new Date().setHours(11, 00);
     const now = new Date();
-    const difference = ((now - initTime) / 60000 ) / 15;
-    const add = difference * 0.02 * 100;
-    val += add;
+    const difference = ((now - initTime) / 60000 ) / 5;
+    const add = difference * 0.104;
+    if (difference > 0) {
+        val += add;
+    }
     const newPercenteage = `${val.toFixed(2)}%`;
 
     progress.style.height = newPercenteage;
